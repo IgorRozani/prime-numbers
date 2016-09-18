6610754bf0d4249792ebaff28b96d30cc248d0c8
igorrozani <igorrozani@gmail.com>
2016-09-18 18:12:18 -0300

    First refactoring

--- prime-numbers.go
+++ prime-numbers.go
@@ -16,13 +16,7 @@ func main() {
 	for len(primeNumbers) < 100 {
 		currentValue++
 
-		isDivisible := false
-		for _, value := range primeNumbers {
-			if math.Mod(float64(currentValue), float64(value)) == 0 {
-				isDivisible = true
-				break
-			}
-		}
+		isDivisible := isDivisible(currentValue, primeNumbers)
 
 		if !isDivisible {
 			primeNumbers = append(primeNumbers, currentValue)
@@ -31,4 +25,15 @@ func main() {
 	}
 
 	fmt.Println(primeNumbers)
+}
+
+func isDivisible(number int, primeNumbers []int) bool {
+	isDivisible := false
+	for _, value := range primeNumbers {
+		if math.Mod(float64(number), float64(value)) == 0 {
+			isDivisible = true
+			break
+		}
+	}
+	return isDivisible
 }
